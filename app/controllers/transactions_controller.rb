75d1786f8870f88757a9f4fc630c3cc701385d01
Slavapat5 <erikpompov@gmail.com>
2025-10-30 13:03:30 +0000

    Fix controller class definition

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,49 +1,55 @@
-def create
-  # Associate the new transaction with the currently logged-in user
-  @transaction = current_user.transactions.new(transaction_params)
+class TransactionsController < ApplicationController
+  before_action :set_transaction, only: %i[show edit update destroy]
 
-  respond_to do |format|
-    if @transaction.save
-      format.html { redirect_to @transaction, notice: "Transaction was successfully created." }
-      format.json { render :show, status: :created, location: @transaction }
-    else
-      format.html { render :new, status: :unprocessable_entity }
-      format.json { render json: @transaction.errors, status: :unprocessable_entity }
+  # POST /transactions or /transactions.json
+  def create
+    # Associate the new transaction with the currently logged-in user
+    @transaction = current_user.transactions.new(transaction_params)
+
+    respond_to do |format|
+      if @transaction.save
+        format.html { redirect_to @transaction, notice: "Transaction was successfully created." }
+        format.json { render :show, status: :created, location: @transaction }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
     end
   end
-end
 
-# PATCH/PUT /transactions/1 or /transactions/1.json
-def update
-  respond_to do |format|
-    if @transaction.update(transaction_params)
-      format.html { redirect_to @transaction, notice: "Transaction was successfully updated.", status: :see_other }
-      format.json { render :show, status: :ok, location: @transaction }
-    else
-      format.html { render :edit, status: :unprocessable_entity }
-      format.json { render json: @transaction.errors, status: :unprocessable_entity }
+  # PATCH/PUT /transactions/1 or /transactions/1.json
+  def update
+    respond_to do |format|
+      if @transaction.update(transaction_params)
+        format.html { redirect_to @transaction, notice: "Transaction was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @transaction }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @transaction.errors, status: :unprocessable_entity }
+      end
     end
   end
+
+  # DELETE /transactions/1 or /transactions/1.json
+  def destroy
+    @transaction.destroy  # Changed from @transaction.destroy! to @transaction.destroy
+
+    respond_to do |format|
+      format.html { redirect_to transactions_path, notice: "Transaction was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_transaction
+      @transaction = Transaction.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def transaction_params
+      params.require(:transaction).permit(:occurred_on, :amount, :note, :category_id)
+    end
 end
 
-# DELETE /transactions/1 or /transactions/1.json
-def destroy
-  @transaction.destroy!
-
-  respond_to do |format|
-    format.html { redirect_to transactions_path, notice: "Transaction was successfully destroyed.", status: :see_other }
-    format.json { head :no_content }
-  end
-end
-
-private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_transaction
-    @transaction = Transaction.find(params[:id])  # Corrected this line
-  end
-
-  # Only allow a list of trusted parameters through.
-  def transaction_params
-    params.require(:transaction).permit(:occurred_on, :amount, :note, :category_id)  # Fixed `expect` to `permit`
-  end

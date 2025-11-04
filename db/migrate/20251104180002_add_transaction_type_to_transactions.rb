93c36d0296f0e41439e6ac0d76cabefdb7eb6292
Slavapat5 <erikpompov@gmail.com>
2025-11-04 21:10:24 +0000

    Fix Transaction model and add transaction_type enum

--- db/migrate/20251104180002_add_transaction_type_to_transactions.rb
+++ db/migrate/20251104180002_add_transaction_type_to_transactions.rb
@@ -1,5 +1,13 @@
-class AddTransactionTypeToTransactions < ActiveRecord::Migration[8.0]
+class CreateTransactions < ActiveRecord::Migration[8.0]
   def change
-    add_column :transactions, :transaction_type, :integer, default: 0, null: false
+    create_table :transactions do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :category, null: false, foreign_key: true
+      t.integer :transaction_type, null: false, default: 0
+      t.decimal :amount, precision: 12, scale: 2, null: false
+      t.date :occurred_on, null: false
+
+      t.timestamps
+    end
   end
 end

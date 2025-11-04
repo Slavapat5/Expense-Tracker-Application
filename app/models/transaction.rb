36983c7a60e917aa1878edbe44fa9119059a9aa5
Slavapat5 <erikpompov@gmail.com>
2025-11-04 18:16:47 +0000

    Add transaction_type enum to transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,8 +2,11 @@ class Transaction < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
+  # Enum for transaction type (mapped to integer column)
   enum transaction_type: { expense: 0, income: 1 }
 
+  # Validations
   validates :amount, presence: true, numericality: true
   validates :occurred_on, presence: true
+  validates :transaction_type, presence: true
 end

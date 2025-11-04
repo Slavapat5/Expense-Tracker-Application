f5fb92df962d935302e1344de3b41432a8fb3583
Slavapat5 <erikpompov@gmail.com>
2025-11-04 21:48:15 +0000

    fix enum loading

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,11 +2,11 @@ class Transaction < ApplicationRecord
   belongs_to :category
   belongs_to :user
 
-  validates :amount, presence: true, numericality: true
-  validates :occurred_on, presence: true
-
   # Only define enum if the column exists
   if column_names.include?("transaction_type")
     enum transaction_type: { expense: 0, income: 1 }
   end
+
+  validates :amount, presence: true, numericality: true
+  validates :occurred_on, presence: true
 end

0064dbc7d62e7d92882af591553966b95a681108
Slavapat5 <erikpompov@gmail.com>
2025-11-12 14:34:45 +0000

    New Line Graph

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,7 +12,13 @@ class DashboardController < ApplicationController
       .where(occurred_on: Date.current.beginning_of_month..Date.current.end_of_month)
       .sum(:amount)
 
-    # Spending by category
+    # Spending last month
+    last_month_range = (Date.current.last_month.beginning_of_month..Date.current.last_month.end_of_month)
+    @last_month_spent = @transactions
+      .where(occurred_on: last_month_range)
+      .sum(:amount)
+
+    # Spending by category (for pie chart)
     @spending_by_category = @transactions
       .joins(:category)
       .group("categories.name")
@@ -20,5 +26,24 @@ class DashboardController < ApplicationController
 
     # Recent transactions
     @recent_transactions = @transactions.order(occurred_on: :desc).limit(5)
+
+    # Monthly spending (for line chart)
+    monthly_data = @transactions.group_by { |t| t.occurred_on.beginning_of_month }
+    @months = monthly_data.keys.sort.map { |date| date.strftime("%b %Y") }
+    @monthly_totals = monthly_data.keys.sort.map { |date| monthly_data[date].sum(&:amount) }
+
+    # Spending insights
+    if @last_month_spent > 0
+      change = ((@month_spent - @last_month_spent) / @last_month_spent.to_f * 100).round(1)
+      if change > 0
+        @insight_message = "You spent #{change}% more this month than last month."
+      elsif change < 0
+        @insight_message = "You spent #{change.abs}% less this month than last month."
+      else
+        @insight_message = "Your spending is the same as last month."
+      end
+    else
+      @insight_message = "No spending data for last month to compare."
+    end
   end
 end
